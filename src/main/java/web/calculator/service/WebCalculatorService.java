e60a897b41103fdba79cf761f775b9c16edbfd0e
Ovcharenko Oleksandr <ovcharenko.messor@gmail.com>
2022-02-18 11:35:31 +0200

    add test for controller add localization

--- src/main/java/web/calculator/service/WebCalculatorService.java
+++ src/main/java/web/calculator/service/WebCalculatorService.java
@@ -71,5 +71,4 @@ public class WebCalculatorService implements CalculatorService {
 
         return sum * step;
     }
-
 }
--- src/test/java/web/calculator/controller/MainControllerTest.java
+++ src/test/java/web/calculator/controller/MainControllerTest.java
@@ -15,9 +15,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import web.calculator.service.WebCalculatorService;
 
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
@@ -34,8 +35,8 @@ public class MainControllerTest {
 
     @Before
     public void setUp() {
-        MainController controller = new MainController(calculatorService); // 1
-        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build(); // 2
+        MainController controller = new MainController(calculatorService);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
@@ -47,22 +48,66 @@ public class MainControllerTest {
     }
 
     @Test
-    @DisplayName("Test go to main page by path /calculate")
+    @DisplayName("Test should show correct answer if function is correct")
     public void calculateFunctionWithCorrectInputsTest() throws Exception {
         double rectangleResult = calculatorService.integrateRectangleMethod(0 ,2, "x^2");
         double trapezoidalResult = calculatorService.integrateTrapezoidalMethod(0 ,2, "x^2");
         double simpsonRuleResult = calculatorService.integrateSimpsonMethod(0 ,2, "x^2");
         double parabolasResult = calculatorService.integrateParabolasMethod(0 ,2, "x^2");
 
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/calculate")
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/integrate")
                 .param("function", "x^2")
+                .param("numFrom", "0")
+                .param("numTo", "2"))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.flash().attribute("rectangleResult", rectangleResult))
+                .andExpect(MockMvcResultMatchers.flash().attribute("trapezoidalResult", trapezoidalResult))
+                .andExpect(MockMvcResultMatchers.flash().attribute("parabolasResult", parabolasResult))
+                .andExpect(redirectedUrl("/"));
+
+        double epsilon = 0.01d;
+        assertEquals(simpsonRuleResult, simpsonRuleResult, epsilon);
+    }
+
+    @Test
+    @DisplayName("Test should show correct answer if trigonometrical function is correct")
+    public void calculateTrigonometricalFunctionWithCorrectInputsTest() throws Exception {
+        double rectangleResult = calculatorService.integrateRectangleMethod(0 ,2, "cos(x^2)");
+        double trapezoidalResult = calculatorService.integrateTrapezoidalMethod(0 ,2, "cos(x^2)");
+        double simpsonRuleResult = calculatorService.integrateSimpsonMethod(0 ,2, "cos(x^2)");
+        double parabolasResult = calculatorService.integrateParabolasMethod(0 ,2, "cos(x^2)");
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/integrate")
+                .param("function", "cos(x^2)")
                 .param("numFrom", "0.0")
-                .param("numTo", "0.2"))
+                .param("numTo", "2.0"))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.flash().attribute("rectangleResult", rectangleResult))
+                .andExpect(MockMvcResultMatchers.flash().attribute("trapezoidalResult", trapezoidalResult))
+                .andExpect(MockMvcResultMatchers.flash().attribute("parabolasResult", parabolasResult))
+                .andExpect(redirectedUrl("/"));
+
+        double epsilon = 0.01d;
+        assertEquals(simpsonRuleResult, simpsonRuleResult, epsilon);
+    }
+
+    @Test
+    @DisplayName("Test should show NaN when incorrect result")
+    public void showNaNWhenPutIncorrectFunction() throws Exception {
+        double rectangleResult = calculatorService.integrateRectangleMethod(0 ,2, "x2");
+        double trapezoidalResult = calculatorService.integrateTrapezoidalMethod(0 ,2, "x2");
+        double simpsonRuleResult = calculatorService.integrateSimpsonMethod(0 ,2, "x2");
+        double parabolasResult = calculatorService.integrateParabolasMethod(0 ,2, "x2");
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/integrate")
+                .param("function", "x2")
+                .param("numFrom", "0.0")
+                .param("numTo", "2.0"))
                 .andDo(print())
                 .andExpect(MockMvcResultMatchers.flash().attribute("rectangleResult", rectangleResult))
                 .andExpect(MockMvcResultMatchers.flash().attribute("trapezoidalResult", trapezoidalResult))
                 .andExpect(MockMvcResultMatchers.flash().attribute("simpsonRuleResult", simpsonRuleResult))
                 .andExpect(MockMvcResultMatchers.flash().attribute("parabolasResult", parabolasResult))
-                .andExpect(status().isOk());
+                .andExpect(redirectedUrl("/"));
     }
 }
